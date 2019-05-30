e428370e639a9f254be7aac6dd14b3260caca476
yuryvoschilo <yury.vashchyla@moneyman.ru>
2019-05-30 10:06:40 +0300

    add jdbc exmaple

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 https://spring.io
 
 https://www.mkyong.com/struts/struts-spring-integration-example/
+
+https://www.baeldung.com/
---
+++ src/main/resources/task2/create.sql
@@ -0,0 +1,12 @@
+CREATE SCHEMA IF NOT EXISTS test;
+SET SCHEMA test;
+
+create table book (
+  id              NUMBER(7)     NOT NULL PRIMARY KEY,
+  name            VARCHAR2(50)       NOT NULL
+);
+
+insert into book (id, name) values (1, 'Book1');
+insert into book (id, name) values (2, 'Book2');
+insert into book (id, name) values (3, 'Book3');
+insert into book (id, name) values (4, 'Book4');
