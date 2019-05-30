0d21af9dd65cba8cc58070f9cc781b4bc7970051
valery <valery@valery-desktop>
2019-05-30 20:46:48 +0300

    work home

--- src/main/java/by/cources/spring/task2/spring/BookConfig.java
+++ src/main/java/by/cources/spring/task2/spring/BookConfig.java
@@ -10,30 +10,30 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-
 @Configuration
 @PropertySource("classpath:task2/database.properties")
 public class BookConfig {
 
-  @Autowired
-  Environment environment;
+	@Autowired
+	Environment environment;
 
-  @Bean
-  public BookRepository jdbcRepository() {
-    return new JdbcTemplateBookRepository(dataSource());
-  }
+	@Bean
+	public BookRepository jdbcRepository() {
+		return new JdbcTemplateBookRepository(dataSource());
+	}
 
-  @Bean
-  DataSource dataSource() {
-    DriverManagerDataSource source = new DriverManagerDataSource();
-    source.setUrl(environment.getProperty("url"));
-    source.setUsername(environment.getProperty("dbuser"));
-    source.setPassword(environment.getProperty("dbpassword"));
-    source.setDriverClassName(environment.getProperty("driver"));
-    return source;
-  }
-  @Bean
-  public BookService bookService() {
-    return new BookService(jdbcRepository());
-  }
+	@Bean
+	DataSource dataSource() {
+		DriverManagerDataSource source = new DriverManagerDataSource();
+		source.setUrl(environment.getProperty("url"));
+		source.setUsername(environment.getProperty("dbuser"));
+		source.setPassword(environment.getProperty("dbpassword"));
+		source.setDriverClassName(environment.getProperty("driver"));
+		return source;
+	}
+
+	@Bean
+	public BookService bookService() {
+		return new BookService(jdbcRepository());
+	}
 }
--- src/main/resources/task2/create.sql
+++ src/main/resources/task2/create.sql
@@ -4,10 +4,10 @@ SET SCHEMA test;
 create table book (
   id              NUMBER(7)     NOT NULL PRIMARY KEY,
   name            VARCHAR2(50)  NOT NULL,
-  year            DATE          NOT NULL
+  year            NUMBER(4)     NOT NULL
 );
 
-insert into book (id, name) values (1, 'Book1');
-insert into book (id, name) values (2, 'Book2');
-insert into book (id, name) values (3, 'Book3');
-insert into book (id, name) values (4, 'Book4');
+insert into book (id, name) values (1, 'Book1', 2015);
+insert into book (id, name) values (2, 'Book2', 2000);
+insert into book (id, name) values (3, 'Book3', 1965);
+insert into book (id, name) values (4, 'Book4', 1985);
