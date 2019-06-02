66d0678b6b51914d8b692248463b8078ddc6c78b
Sharaiandrei <andrei_sharai@mail.ru>
2019-06-03 00:12:47 +0300

    2

--- src/main/resources/task2/create.sql
+++ src/main/resources/task2/create.sql
@@ -3,18 +3,20 @@ SET SCHEMA test;
 
 create table book (
   id              NUMBER(7)     NOT NULL PRIMARY KEY,
-  name            VARCHAR2(50)       NOT NULL
+  name            VARCHAR2(50)       NOT NULL,
+  YEAR            NUMBER(4)     NOT NULL
 );
 
-insert into book (id, name) values (1, 'Book1');
-insert into book (id, name) values (2, 'Book2');
-insert into book (id, name) values (3, 'Book3');
-insert into book (id, name) values (4, 'Book4');
-insert into book (id, name) values (5, 'Book5');
+insert into book (id, name) values (1, 'Book1',1963);
+insert into book (id, name) values (2, 'Book2',1964);
+insert into book (id, name) values (3, 'Book3',1965);
+insert into book (id, name) values (4, 'Book4',1966);
+insert into book (id, name) values (5, 'Book5',1967);
 
 create table Author (
   id              NUMBER(7)     NOT NULL PRIMARY KEY,
-  name            VARCHAR2(50)       NOT NULL
+  name            VARCHAR2(50)       NOT NULL,
+  Bithday         DATE           NOT NULL
 );
 
 insert into Author (id, name) values (1, 'Author1');
