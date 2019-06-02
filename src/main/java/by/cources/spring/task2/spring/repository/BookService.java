fe3d42917da967ad94b23e9c04b217bf9e705510
Sharaiandrei <andrei_sharai@mail.ru>
2019-06-03 01:17:13 +0300

    2

---
+++ src/main/java/by/cources/spring/task2/spring/repository/BookService.java
@@ -0,0 +1,19 @@
+package by.cources.spring.task2.spring.repository;
+
+import by.cources.spring.task2.spring.model.Book;
+import by.cources.spring.task2.spring.repository.BookRepository;
+
+import java.util.List;
+
+public class BookService {
+
+  private BookRepository repository;
+
+  public BookService(BookRepository repository) {
+    this.repository = repository;
+  }
+
+  public List<Book> findAll() {
+    return repository.findAll();
+  }
+}
