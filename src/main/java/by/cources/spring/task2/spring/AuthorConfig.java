374f68259f9d0a1f50830b7a4cf829b140970ed8
Sharaiandrei <andrei_sharai@mail.ru>
2019-06-03 11:11:21 +0300

    AuthorConfig

---
+++ src/main/java/by/cources/spring/task2/spring/AuthorConfig.java
@@ -0,0 +1,45 @@
+package by.cources.spring.task2.spring;
+
+
+import by.cources.spring.task2.spring.repository.AuthorRepository;
+import by.cources.spring.task2.spring.repository.AuthorService;
+import by.cources.spring.task2.spring.repository.JdbcTemplateAuthorRepository;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+
+
+@Configuration
+@PropertySource("classpath:task2/database.properties")
+public class AuthorConfig {
+
+  @Autowired
+  Environment environment;
+
+  @Bean
+  AuthorRepository jdbcRepository() {
+    return new JdbcTemplateAuthorRepository(dataSource());
+  }
+
+  @Bean
+  AuthorService authorService{
+    AuthorService authorService1 = new AuthorService(jdbcRepository());
+    return authorService1;
+  }
+
+  @Bean
+  DataSource dataSource() {
+    DriverManagerDataSource source = new DriverManagerDataSource();
+    source.setUrl(environment.getProperty("url"));
+    source.setUsername(environment.getProperty("dbuser"));
+    source.setPassword(environment.getProperty("dbpassword"));
+    source.setDriverClassName(environment.getProperty("driver"));
+    return source;
+  }
+
+}
