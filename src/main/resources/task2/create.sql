92054d4e28aa395aaf3938d41e4b9b7850b978c8
Sharaiandrei <andrei_sharai@mail.ru>
2019-06-03 10:18:20 +0300

    1

--- src/main/resources/task2/create.sql
+++ src/main/resources/task2/create.sql
@@ -7,11 +7,11 @@ create table book (
   year            NUMBER(4)     NOT NULL
 );
 
-insert into book (id, name) values (1, 'Book1',1963);
-insert into book (id, name) values (2, 'Book2',1964);
-insert into book (id, name) values (3, 'Book3',1965);
-insert into book (id, name) values (4, 'Book4',1966);
-insert into book (id, name) values (5, 'Book5',1967);
+insert into book  values (1, 'Book1',1963);
+insert into book  values (2, 'Book2',1964);
+insert into book  values (3, 'Book3',1965);
+insert into book  values (4, 'Book4',1966);
+insert into book  values (5, 'Book5',1967);
 
 create table Author (
   id              NUMBER(7)     NOT NULL PRIMARY KEY,
@@ -19,8 +19,8 @@ create table Author (
   bithday         DATE           NOT NULL
 );
 
-insert into Author (id, name) values (1, 'Author1','1932-10-01');
-insert into Author (id, name) values (2, 'Author2','1943-02-23');
-insert into Author (id, name) values (3, 'Author3','1921-01-24');
-insert into Author (id, name) values (4, 'Author4','1945-05-09');
-insert into Author (id, name) values (5, 'Author5','1932-06-07');
+insert into Author  values (1, 'Author1','1932-10-01');
+insert into Author  values (2, 'Author2','1943-02-23');
+insert into Author  values (3, 'Author3','1921-01-24');
+insert into Author  values (4, 'Author4','1945-05-09');
+insert into Author  values (5, 'Author5','1932-06-07');
