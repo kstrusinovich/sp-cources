11159fa1eeb03fc71c51e822975db7ae3ecb9c7a
valeryKonchevich <DFKthf1803>
2019-06-03 11:28:25 +0300

    add model language

---
+++ src/main/java/by/cources/spring/task3/spring/repository/LanguageRepository.java
@@ -0,0 +1,16 @@
+package by.cources.spring.task3.spring.repository;
+
+import by.cources.spring.task3.spring.model.Language;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface LanguageRepository extends CrudRepository<Language, Long> {
+
+  Optional<Language> findById(Long id);
+
+  List<Language> findAll();
+}
