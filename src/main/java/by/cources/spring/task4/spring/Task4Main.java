6846f5541decc24d64dccfc88f438a5a5fdf7bbc
shpakovsky1992 <shpakovsky1992@gmail.com>
2019-06-05 11:34:12 +0300

    zadanie4

---
+++ src/main/java/by/cources/spring/task4/spring/Task4Main.java
@@ -0,0 +1,44 @@
+package by.cources.spring.task4.spring;
+
+import by.cources.spring.task4.spring.model.Author;
+import by.cources.spring.task4.spring.model.Book;
+import by.cources.spring.task4.spring.service.BookService;
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Task4Main {
+
+  public static void main(String[] args) {
+    ApplicationContext ctx = new AnnotationConfigApplicationContext(BookConfig.class);
+    BookService service = ctx.getBean(BookService.class);
+    System.out.println("---------------------");
+    Author author = new Author();
+
+    author.setLastName("lastName");
+    author.setFirstName("firstName");
+    author.setDateOfBirth(LocalDate.now());
+    service.saveAuthor(author);
+    System.out.println("Saved author with id = " + author.getId());
+    System.out.println("---------------------");
+    query5(service);
+    Book book = new Book();
+    book.setName("Challenger");
+    service.saveBook(book);
+    query1(service);
+  }
+
+  private static void query1(BookService service) {
+    List<Book> books = service.findBooksAll();
+    for (Book book : books) {
+      System.out.println("Query1: " + book);
+    }
+  }
+  private static void query5(BookService service) {
+    List<Author> authors = service.findAuthorsAll();
+    for (Author author : authors) {
+      System.out.println("Query5: " + author);
+    }
+  }
+}
