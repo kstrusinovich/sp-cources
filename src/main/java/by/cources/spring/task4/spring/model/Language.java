fa3ea759d1900622db05aabf01f00361e87c73e3
student <student@DOMAINADMINS8.testcenter.local>
2019-06-05 11:30:25 +0300

    adding task4

---
+++ src/main/java/by/cources/spring/task4/spring/model/Language.java
@@ -0,0 +1,40 @@
+package by.cources.spring.task4.spring.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "language")
+public class Language {
+	
+	@Id
+	@Column(name = "id")
+	private Long id;
+	
+
+	@Column(name = "name")	
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
+	@Override
+	public String toString () {
+		return "Language{id=" + id + "; language="+name+"}";
+	}
+
+}
