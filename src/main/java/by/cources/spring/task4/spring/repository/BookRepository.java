7bafb3e4f670d691c001d8e7db3dc957e6bfa05a
Student <suhlesya>
2019-06-05 11:30:14 +0300

    comment2-1

--- src/main/java/by/cources/spring/task4/spring/repository/BookRepository.java
+++ src/main/java/by/cources/spring/task4/spring/repository/BookRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface BookRepository extends CrudRepository<Book, Long> {
