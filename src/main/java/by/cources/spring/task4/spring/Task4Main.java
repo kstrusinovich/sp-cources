3525b1578389aee4e2abb388960fff46ace45760
MARIA <s_mar_s@mail.ru>
2019-06-07 17:58:26 +0300

    task4 saveBook finished

--- src/main/java/by/cources/spring/task4/spring/Task4Main.java
+++ src/main/java/by/cources/spring/task4/spring/Task4Main.java
@@ -24,41 +24,22 @@ public class Task4Main {
     System.out.println("ALL AUTHORS---------------------");
     query5(service);
 
-    Language language = new Language();
-    language.setName("russian");
-
-    Book book = new Book();
-    book.setName("BookName1");
-    book.setPublishedIn(1995L);
-    book.setAuthor(author);
-    book.setLanguage(language);
+    Book book= createBook("BookName1",author,1995L,"spanish");
     service.saveBook(book);
     System.out.println("Saved book with id = " + book.getId()+" name="+book.getName()+
                       " author="+book.getAuthor()+" language="+book.getLanguage()+
                       " PublishedIn="+book.getPublishedIn());
+    book= createBook("BookName2",author,1595L,"french");
+    service.saveBook(book);
+    System.out.println("Saved book with id = " + book.getId()+" name="+book.getName()+
+                       " author="+book.getAuthor()+" language="+book.getLanguage()+
+                       " PublishedIn="+book.getPublishedIn());
+    book= createBook("BookName3",author,1885L,"russian");
+    service.saveBook(book);
+    System.out.println("Saved book with id = " + book.getId()+" name="+book.getName()+
+                       " author="+book.getAuthor()+" language="+book.getLanguage()+
+                       " PublishedIn="+book.getPublishedIn());
 
-  /*  Book book2 = new Book();
-    book2.setName("BookName2");
-    book2.setPublishedIn(1895L);
-    book2.setAuthor(author);
-    book2.setLanguage(language);
-    service.saveBook(book2);
-    System.out.println("Saved book with id = " + book2.getId()+" name="+book2.getName()+
-            " author="+book2.getAuthor()+" language="+book2.getLanguage()+
-            " PublishedIn="+book2.getPublishedIn());
-    Book book3 = new Book();
-    Language language2 = new Language();
-    book3.setName("BookName3");
-    book3.setPublishedIn(1907L);
-    author.setLastName("lastName3");
-    author.setFirstName("firstName3");
-    book3.setAuthor(author);
-    language2.setName("japan");
-    book3.setLanguage(language2);
-    service.saveBook(book3);
-    System.out.println("Saved book with id = " + book3.getId()+" name="+book3.getName()+
-            " author="+book3.getAuthor()+" language="+book3.getLanguage()+
-            " PublishedIn="+book3.getPublishedIn());*/
 
     System.out.println(" ALL LANGUAGES---------------------");
     query6(service);
@@ -66,7 +47,16 @@ public class Task4Main {
     query1(service);
 
   }
-
+  private static Book createBook(String name,Author author,Long published,String lang) {
+     Language language = new Language();
+    language.setName(lang);
+    Book book = new Book();
+    book.setName(name);
+    book.setPublishedIn(published);
+    book.setAuthor(author);
+    book.setLanguage(language);
+   return book;
+  }
   private static void query5(BookService service) {
     List<Author> authors = service.findAuthorsAll();
     for (Author author : authors) {
