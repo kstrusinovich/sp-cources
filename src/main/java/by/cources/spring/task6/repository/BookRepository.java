31eb2e46f859b5fabcf9ef5c246071f5a1f30ab7
shpakovsky1992 <shpakovsky1992@gmail.com>
2019-06-14 13:24:15 +0300

    6

--- src/main/java/by/cources/spring/task6/repository/BookRepository.java
+++ src/main/java/by/cources/spring/task6/repository/BookRepository.java
@@ -14,6 +14,4 @@ public interface BookRepository extends CrudRepository<Book, Long> {
   List<Book> findOlderThan(@Param("value") Long value);
 
   List<Book> findByName(String name);
-
-  Book getByBookId(Long id);
 }
